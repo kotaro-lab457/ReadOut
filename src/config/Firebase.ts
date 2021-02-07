36f8c514d2a882b65ca8499a2ccdf893494258a9
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-02-07 21:38:12 +0900

    firebase設定

---
+++ src/config/Firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBehENZGmPCb7IjGGaaJoZws8iAVvzHMQc",
+    authDomain: "my-portfolio-986db.firebaseapp.com",
+    projectId: "my-portfolio-986db",
+    storageBucket: "my-portfolio-986db.appspot.com",
+    messagingSenderId: "1044178099208",
+    appId: "1:1044178099208:web:6503ba091332208d6545bd",
+    measurementId: "G-GLV1SQT78Z"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
