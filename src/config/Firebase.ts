8720f2bb51817c19316de971cb18df865f4c0462
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-03-09 14:05:30 +0900

    Firebase設定

--- src/config/Firebase.ts
+++ src/config/Firebase.ts
@@ -1,18 +1,19 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-import 'firebase/app'
-import 'firebase/auth'
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/app";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBehENZGmPCb7IjGGaaJoZws8iAVvzHMQc",
-    authDomain: "my-portfolio-986db.firebaseapp.com",
-    projectId: "my-portfolio-986db",
-    storageBucket: "my-portfolio-986db.appspot.com",
-    messagingSenderId: "1044178099208",
-    appId: "1:1044178099208:web:6503ba091332208d6545bd",
-    measurementId: "G-GLV1SQT78Z"
-}
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
-export default firebase
+export default firebase;
