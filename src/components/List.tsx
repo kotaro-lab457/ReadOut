f5e7d2cc98436186314b33e3e385b54cfb54400a
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-05-22 10:58:15 +0900

    コメント機能 #106

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../config/Firebase";
 import moment from "moment";
 
+import { COMMENT } from "../module/Text.module";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Img, ImageTag } from "../ui/atoms/image";
-import { TimeFont, Font } from "../ui/atoms/font";
+import {
+  Font,
+  CommentFont,
+  TimeFont,
+  CommentUserFont,
+  CommentsFont,
+} from "../ui/atoms/font";
 import { IconButton } from "../ui/atoms/button";
-import { TableItem, TableTexts } from "../ui/molecules/TableHome";
+import {
+  TableItem,
+  TableTexts,
+  TableCommentsText,
+} from "../ui/molecules/TableHome";
 import { TableIcon } from "../ui/molecules/TableProfile";
 
 interface listProps {
@@ -24,7 +35,31 @@ interface listProps {
 }
 
 const List: React.FC<listProps> = (props) => {
-  const FS = firebase.firestore().collection("text");
+  const [count, setCount] = useState<number>(0);
+  const [comments, setComments] = useState<COMMENT[]>([]);
+  const [openComments, setOpenComments] = useState(false);
+
+  const FS = firebase
+    .firestore()
+    .collection("text")
+    .doc(`${props.list.id}`)
+    .collection("comments");
+
+  useEffect(() => {
+    const unSub = FS.orderBy("createAt", "asc").onSnapshot((snapshot) => {
+      const posts: any = snapshot.docs.map((doc) => {
+        return doc.data();
+      });
+      setComments(posts);
+    });
+
+    FS.get().then((doc) => {
+      setCount(doc.size);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
 
   // 編集切り替え ＝ editingを false から true へ
   const handleEditChange = () => {
@@ -44,6 +79,24 @@ const List: React.FC<listProps> = (props) => {
         <TimeFont>
           {moment(props.list.createAt).format("A HH:mm YYYY/MM/DD")}
         </TimeFont>
+        <div>
+          <CommentFont onClick={() => setOpenComments(!openComments)}>
+            コメント（{count}）{openComments ? "非表示" : "表示"}
+          </CommentFont>
+        </div>
+        {openComments && (
+          <div>
+            {comments.map((list, id) => (
+              <TableCommentsText key={id}>
+                <div>
+                  <CommentUserFont>@{list.user}</CommentUserFont>
+                  <TimeFont>{moment(list.createAt).fromNow()}</TimeFont>
+                </div>
+                <CommentsFont>{list.text}</CommentsFont>
+              </TableCommentsText>
+            ))}
+          </div>
+        )}
       </TableTexts>
       <TableIcon>
         <IconButton onClick={handleEditChange}>
