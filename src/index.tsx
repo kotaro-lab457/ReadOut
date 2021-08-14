6639cd7b70bd86e7dc1946d130e0182e8dd333ab
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-08-14 10:33:06 +0900

    change: indexから base.cssへ変更（aクラス,font-sizeの追加）

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import Router from "./router/router";
 import { Provider } from "react-redux";
 import { store } from "./stores/store";
-import "./css/index.css";
+import "./css/base.css";
 
 ReactDOM.render(
   <Provider store={store}>
