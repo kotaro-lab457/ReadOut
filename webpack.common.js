1c360539aa7ba9a9a416578e8e2dbc5e377ff2f4
kotaro-lab457 <k.chanmpmp@icloud.com>
2022-02-07 00:13:50 +0900

    設定ファイルの改善

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = () => {
       bundle: "./src/index.tsx",
     },
     output: {
-      path: path.resolve(__dirname, "build"),
+      path: path.resolve(__dirname, "dist"),
       filename: "js/[name].js",
     },
     resolve: {
